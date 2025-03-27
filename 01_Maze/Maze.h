d096b298f47aebe6d76a449524555844d13eddbd
Freesia <2631266284@qq.com>
2025-03-27 14:37:52 +0800

    加回来

---
+++ 01_Maze/Maze.h
@@ -0,0 +1,12 @@
+/*****************************
+*    Header file for the     *
+*    maze solver             *
+*****************************/
+#ifndef MAZE_H
+#define MAZE_H
+
+bool LoadMaze(const char[]);
+bool LoadMazePointer(const char[]);
+void SolveMaze();
+
+#endif
